48ced5689957df1b460488f462e3d65626a6e603
MarinaBartulov <bartulovmarina1@gmail.com>
2020-05-27 13:22:28 +0200

    Project created.

---
+++ src/main/java/com/team19/rentmicroservice/RentmicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.team19.rentmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentmicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentmicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/team19/rentmicroservice/RentmicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team19.rentmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentmicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
