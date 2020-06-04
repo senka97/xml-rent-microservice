fa8564e075dbd87ba54ca1b9d2d9015542b16cbf
MarinaBartulov <bartulovmarina1@gmail.com>
2020-06-04 14:30:39 +0200

    Data changed.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,5 @@
+insert into cart (clientid) value (3);
+insert into cart_item (adid, start_date, end_date, in_cart, ownerid, cart_id) value (1, '2020-06-04','2020-06-08', false, 2, 1);
 insert into request (clientid, ownerid, status) value (3,2,'Paid');
 insert into request_ad (adid, clientid, ownerid, current_price_per_km, payment, start_date, end_date, request_id) value (1,3,2,3,125,'2020-06-04','2020-06-08',1);
 
